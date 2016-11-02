553c5192fe3c2e684696993ce145dde13f235574
Damien Mayance <dmayance@gmail.com>
2016-11-02 14:29:22 +0100

    prepare ticket generation

---
+++ Assets/scripts/game/TicketScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TicketScript : NetworkBehaviour
+{
+  [SyncVar]
+  public int booth;
+  [SyncVar]
+  public int number;
+
+  public Text numberText;
+
+  void Update()
+  {
+    numberText.text = booth + "-" + number;
+  }
+}
