5cafa0491750e4a4620a056c71033a20b419e2b0
Damien Mayance <dmayance@gmail.com>
2016-11-02 11:44:23 +0100

    save current prototype on github

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerScript : NetworkBehaviour
+{
+  public MeshRenderer model;
+  public FirstPersonController fpsController;
+  public Camera fpsCamera;
+
+  void Start()
+  {
+    if (isLocalPlayer)
+    {
+      // Remove model
+      Destroy(model.gameObject);
+    }
+    else
+    {
+      // Remove FPS Controller
+      Destroy(fpsCamera.gameObject);
+      Destroy(fpsController);
+    }
+  }
+}
