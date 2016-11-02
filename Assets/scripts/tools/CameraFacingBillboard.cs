7f917912ac5cabc4352e0186e177cf44a993b091
Damien Mayance <dmayance@gmail.com>
2016-11-02 14:17:07 +0100

    add the first sprites! with billboard effect

---
+++ Assets/scripts/tools/CameraFacingBillboard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+  void Update()
+  {
+    if (Camera.main != null)
+    {
+      transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+          Camera.main.transform.rotation * Vector3.up);
+    }
+  }
+}
