67622fc17229c618d14f7c7f63ec795a17b07b7c
Damien Mayance <dmayance@gmail.com>
2016-11-03 13:07:16 +0100

    add connect/disconnect sounds

--- Assets/scripts/network/GameNetworkManager.cs
+++ Assets/scripts/network/GameNetworkManager.cs
@@ -4,4 +4,17 @@ using UnityEngine.Networking;
 
 public class GameNetworkManager : NetworkManager
 {
+  public override void OnClientConnect(NetworkConnection conn)
+  {
+    base.OnClientConnect(conn);
+
+    GameServer.PlaySound("player_connect", this.transform.position);
+  }
+
+  public override void OnClientDisconnect(NetworkConnection conn)
+  {
+    base.OnClientDisconnect(conn);
+
+    GameServer.PlaySound("player_disconnect", this.transform.position);
+  }
 }
