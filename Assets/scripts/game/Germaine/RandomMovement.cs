a6e01b0135e9de8937df402cec6b5c219231bded
Damien Mayance <apps>
2016-11-25 09:35:07 +0100

    add Roomba AI to Germaine

---
+++ Assets/scripts/game/Germaine/RandomMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class RandomMovement : NetworkBehaviour
+{
+  public float speed = 0.035f;
+
+  private float targetRotation;
+  private float nextRotationCooldown;
+  private float collisionCooldown;
+
+  [Server]
+  void Start()
+  {
+    nextRotationCooldown = Random.Range(0, 5);
+
+    targetRotation = transform.rotation.eulerAngles.y;
+  }
+
+  [Server]
+  void Update()
+  {
+    transform.position += speed * transform.forward;
+
+    collisionCooldown -= Time.deltaTime;
+    nextRotationCooldown -= Time.deltaTime;
+    if(nextRotationCooldown <0)
+    {
+      nextRotationCooldown = Random.Range(5, 15);
+
+      targetRotation += Random.Range(30, 90);
+    }
+    targetRotation %= 360;
+
+    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Mathf.Lerp(transform.rotation.eulerAngles.y, targetRotation, Time.deltaTime), transform.rotation.eulerAngles.z);
+  }
+
+  void OnCollisionEnter(Collision c)
+  {
+    if (collisionCooldown < 0)
+    {
+      collisionCooldown = 10;
+      nextRotationCooldown = 5;
+      targetRotation = targetRotation + Random.Range(90, 270);
+    }
+  }
+}
