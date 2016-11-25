96eb3e7f96142518c8308b55f448a881244e66b6
Damien Mayance <apps>
2016-11-25 11:54:12 +0100

    optimize billboard

--- Assets/scripts/tools/CameraFacingBillboard.cs
+++ Assets/scripts/tools/CameraFacingBillboard.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class CameraFacingBillboard : MonoBehaviour
 {
-  void Update()
+  private SpriteRenderer sprite;
+
+  void Start()
   {
-    if (Camera.main != null)
+    sprite = GetComponent<SpriteRenderer>();
+    if (sprite != null)
+    {
+      InvokeRepeating("FacePlayer", 1f, 0.1f);
+    }
+  }
+
+  private void FacePlayer()
+  {
+    if (Camera.main != null && sprite != null && sprite.isVisible)
     {
       transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
           Camera.main.transform.rotation * Vector3.up);
+      transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
     }
   }
 }
