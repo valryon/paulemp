03a7d20c9c664cfe01b56eb43215f05921b7459d
Damien Mayance <apps>
2016-11-30 09:40:07 +0100

    add sounds to PNJ

--- Assets/scripts/game/NPC/NPCScript.cs
+++ Assets/scripts/game/NPC/NPCScript.cs
@@ -1,31 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.Networking;
 
-public class RandomMovement : MonoBehaviour
+public class NPCScript : MonoBehaviour
 {
+  #region Members
+
   public float speed = 0.035f;
+  public AudioClip[] randomSounds;
+  public AudioClip[] interactSounds;
 
   private float targetRotation;
   private float nextRotationCooldown;
   private float collisionCooldown;
 
+  private float randomSoundCooldown;
+
+  #endregion
+
+  #region Timeline
+
   void Start()
   {
     nextRotationCooldown = Random.Range(0, 5);
+    randomSoundCooldown = Random.Range(5, 15);
 
     targetRotation = transform.rotation.eulerAngles.y;
 
     name = name.Replace("(Clone)", "");
   }
-  
+
   void FixedUpdate()
   {
     transform.position += speed * transform.forward;
 
     collisionCooldown -= Time.deltaTime;
     nextRotationCooldown -= Time.deltaTime;
-    if(nextRotationCooldown <0)
+    if (nextRotationCooldown < 0)
     {
       nextRotationCooldown = Random.Range(5, 15);
 
@@ -34,6 +43,17 @@ public class RandomMovement : MonoBehaviour
     targetRotation %= 360;
 
     transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Mathf.Lerp(transform.rotation.eulerAngles.y, targetRotation, Time.deltaTime), transform.rotation.eulerAngles.z);
+
+    randomSoundCooldown -= Time.deltaTime;
+    if (randomSoundCooldown < 0)
+    {
+      randomSoundCooldown = Random.Range(2, 15);
+      if (randomSounds.Length > 0)
+      {
+        var s = randomSounds[Random.Range(0, randomSounds.Length)];
+        AudioSource.PlayClipAtPoint(s, transform.position);
+      }
+    }
   }
 
   void OnCollisionEnter(Collision c)
@@ -45,4 +65,15 @@ public class RandomMovement : MonoBehaviour
       targetRotation = targetRotation + Random.Range(90, 270);
     }
   }
+
+  void Interact(PlayerScript p)
+  {
+    if (interactSounds.Length > 0)
+    {
+      var s = interactSounds[Random.Range(0, interactSounds.Length)];
+      AudioSource.PlayClipAtPoint(s, transform.position);
+    }
+  }
+
+  #endregion
 }
